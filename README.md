078c5b62532dff2e47c8dc217ae8d24746d237d5
M1guelzinN <antonio.jobs1397@gmail.com>
2022-07-03 22:01:25 -0300

    eslint

--- README.md
+++ README.md
@@ -5,3 +5,18 @@
  <img src="arquivos/img-readme/calculadora-pronta.jpg">
  
 [github pages do repositório](https://m1guelzinn.github.io/calculadora/)
+
+
+
+<!-- 
+# Que horas são?
+
+Este é um programa simples com a função de mostrar o horário atual.
+Além de mudar o ambiente dinamicamente de acordo com horário.
+(*Com possibilidade de mudar o horário*)
+
+**tecnologias usadas**: HTML/CSS/JS
+
+<img src="itens/img/img-md/resultado-readme1.jpg">
+
+[github pages do repositório](https://m1guelzinn.github.io/horario-do-dia/) -->
--- arquivos/calculo.js
+++ arquivos/calculo.js
@@ -1,24 +1,24 @@
-var mostrar = window.document.querySelector('.resultado')
+const mostrar = window.document.querySelector('.resultado');
 
-function valores(n){
-    mostrar.innerHTML += n
+function valores(n) {
+  mostrar.innerHTML += n;
 }
 
-function calcular(){
-    let res = document.querySelector('.resultado').innerHTML
+function calcular() {
+  const res = document.querySelector('.resultado').innerHTML;
 
-    if(mostrar){
-        mostrar.innerHTML = eval(res)
-    } else {
-         mostrar.innerHTML = 'esta errado'
-    }
+  if (mostrar) {
+    mostrar.innerHTML = eval(res);
+  } else {
+    mostrar.innerHTML = 'esta errado';
+  }
 }
 
-function apaga(){
-    let resul = window.document.querySelector('.resultado').innerHTML
-    document.querySelector('.resultado').innerHTML = resul.substring(0, resul.length -1)
+function apaga() {
+  const resul = window.document.querySelector('.resultado').innerHTML;
+  document.querySelector('.resultado').innerHTML = resul.substring(0, resul.length - 1);
 }
 
-function clean(){
-    mostrar.innerHTML = ''
+function clean() {
+  mostrar.innerHTML = '';
 }
